5eaada2c14bf70f4674f1a93dabe7f1927897f21
wangkui0508 <wangkui0508@hotmail.com>
2021-10-11 19:00:26 +0800

    Change icon

--- src/views/MyTokens.vue
+++ src/views/MyTokens.vue
@@ -1,8 +1,8 @@
 <template>
   <h1>List tokens</h1>
   <div class="normal">
-   <p><input v-model="addr" type="text" placeholder="Please enter an address in HEX format"><p/>
-   <p><button @click="list" style="font-size: 24px">List tokens owned by above address</button><p/>
-   <p><button @click="listmine" style="font-size: 24px">List tokens owned by myself</button><p/>
+   <p><input v-model="addr" type="text" placeholder="Please enter an address in HEX format"></p>
+   <p><button @click="list" style="font-size: 24px">List tokens owned by above address</button></p>
+   <p><button @click="listmine" style="font-size: 24px">List tokens owned by myself</button></p>
   </div>
 </template>
